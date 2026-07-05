a9941fb555b61982c04bf3328967a5692eba4223
Chris Zimmerman <chris@chriszimmerman.net>
2026-07-04 23:43:50 -0400

    Extract seven-day report into its own component

---
+++ app/SevenDay/SevenDay.tsx
@@ -0,0 +1,31 @@
+import ProgressBar from "@/app/ProgressBar/ProgressBar";
+
+export default function SevenDay({ weatherData }) {
+  const minTemps = weatherData.sevenDay.map((d) => d.low);
+  const minTemp = Math.min(...minTemps);
+  const maxTemps = weatherData.sevenDay.map((d) => d.high);
+  const maxTemp = Math.max(...maxTemps);
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return (
+    <>
+      {weatherData.sevenDay.map((item, index) => {
+        return (
+          <div key={index} className="flex">
+            <span
+              style={{ whiteSpace: "nowrap", width: "50px" }}
+            >{`${daysOfWeek[item.date.getUTCDay()]} ${item.date.getUTCDate()}`}</span>
+            <ProgressBar
+              scaleMin={minTemp}
+              scaleMax={maxTemp}
+              min={item.low}
+              max={item.high}
+              labelMin={item.low}
+              labelMax={item.high}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,11 @@
 import "./globals.css";
 import { useState } from "react";
 import { getLatLong, getWeather } from "@/app/weather";
-import ProgressBar from "@/app/ProgressBar/ProgressBar";
+import SevenDay from "@/app/SevenDay/SevenDay";
 
 export default function Home() {
   const [zipCode, setZipCode] = useState("19083");
-  const [locationData, setLocationData] = useState({
-    longitude: 0,
-    latitude: 0,
-    displayName: "Null Island",
-  });
+  const [locationData, setLocationData] = useState(null);
   const [weatherData, setWeatherData] = useState({
     daily: [],
     sevenDay: [],
@@ -18,20 +14,14 @@ export default function Home() {
 
   const updateWeather = async () => {
     const updatedLocationData = await getLatLong(zipCode);
-    setLocationData(updatedLocationData);
     const newWeatherData = await getWeather(
-      locationData.latitude,
-      locationData.longitude,
+      updatedLocationData.latitude,
+      updatedLocationData.longitude,
     );
 
+    setLocationData(updatedLocationData);
     setWeatherData(newWeatherData);
   };
-
-  const minTemps = weatherData.sevenDay.map((d) => d.low);
-  const minTemp = Math.min(...minTemps);
-  const maxTemps = weatherData.sevenDay.map((d) => d.high);
-  const maxTemp = Math.max(...maxTemps);
-  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return (
     <>
@@ -39,9 +29,11 @@ export default function Home() {
         <div className="flex justify-center">
           <h1 className="text-4xl">Seven Day Weather Forecast</h1>
         </div>
-        <div className="flex justify-center items-center text-center">
-          <h2 className="text-2xl">{locationData.displayName}</h2>
-        </div>
+        {locationData && (
+          <div className="flex justify-center items-center text-center">
+            <h2 className="text-2xl">{locationData.displayName}</h2>
+          </div>
+        )}
         <div className="flex flex-col gap-4">
           <input
             className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -63,23 +55,7 @@ export default function Home() {
             Fetch weather
           </button>
 
-          {weatherData.sevenDay.map((item, index) => {
-            return (
-              <div key={index} className="flex">
-                <span
-                  style={{ whiteSpace: "nowrap", width: "50px" }}
-                >{`${daysOfWeek[item.date.getUTCDay()]} ${item.date.getUTCDate()}`}</span>
-                <ProgressBar
-                  scaleMin={minTemp}
-                  scaleMax={maxTemp}
-                  min={item.low}
-                  max={item.high}
-                  labelMin={item.low}
-                  labelMax={item.high}
-                />
-              </div>
-            );
-          })}
+          {weatherData && <SevenDay weatherData={weatherData} />}
         </div>
         <div className="flex mt-2 justify-center">
           <a href="https://open-meteo.com/">Weather data by Open-Meteo.com</a>
